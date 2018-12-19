2b1ac331976d103a09565d0304e7554d1ff69f1a
Bharathi S <bharathi.selvarajan@gmail.com>
2018-12-19 14:45:35 +0530

    Update method added

--- playground/mongodb_update.js
+++ playground/mongodb_update.js
@@ -7,7 +7,10 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp",(err,client)=>{
  console.log("Connection eastablished with MongoDb server.");
  const db = client.db('TodoApp');
 
- db.collection('User').deleteOne({name:'Reena'}).then((res)=>{
+ db.collection('User').updateOne({
+     name:'John'},
+    {$inc:{age:2}},
+ ).then((res)=>{
   console.log(res);
  }
  )
