1f9fb9946f1b9fe6f4520f2c65caeca9ea0a8e11
Bharathi S <bharathi.selvarajan@gmail.com>
2018-12-29 07:55:28 +0530

    route todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -18,6 +19,31 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+     res.send({todos});
+  },(e)=>{
+     res.status(400).send(e);
+  })
+  })
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+   if(!todo){
+     return res.status(404).send();
+   }
+   res.send({todo});
+  }).catch((e)=>{
+    res.status(404).send();
+  })
+
+})
 app.post('/user',(req,res)=>{
   var user = new User({
     user:req.body.user,
@@ -31,9 +57,12 @@ app.post('/user',(req,res)=>{
   });
 });
 
+
 app.listen(3000,()=>{
   console.log('starting on port 3000');
 });
+
+//modules.export={app};
 
 
 
